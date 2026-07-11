38992bc708272d8758745f96a43f767524d7c4ec
Jose Reyes Arana Delgado <pepearana974@gmail.com>
2026-07-10 20:20:36 -0600

    Agrega casts mutators accessors y soft deletes

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,9 +6,11 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
+    use SoftDeletes;
     /**
      * Nombre personalizado de la tabla.
      */
